f56ceee97df048d7cc1dfabb9259f04a53a38f18
Baldur van Lew <b.van_lew@lumc.nl>
2024-05-17 15:41:00 +0200

    fix import path

--- python/get_compatibility_list.py
+++ python/get_compatibility_list.py
@@ -23,7 +23,7 @@ def get_list_from_conanfile(args):
     print(f"import path {import_path}")
     print(f"working dir {Path(os.curdir).absolute()}")
     sys.path.append("..")
-    call_func(f'..conanfile', 'compatibility', args.os, args.compiler, args.compiler_version)
+    call_func(f'conanfile', 'compatibility', args.os, args.compiler, args.compiler_version)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
